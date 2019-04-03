ea694efa80095b0380a5b5c811eac8811f9abb91
Christian Wiegand <christian.wiegand@fiduciagad.de>
2019-04-03 16:20:20 +0200

    Changed translations and added locale to manifest

--- mezzanine_blocks/models.py
+++ mezzanine_blocks/models.py
@@ -11,7 +11,7 @@ from .category import BlockCategory
 class BaseBlock(Slugged):
     """Base Block
     """
-    category = models.ForeignKey(BlockCategory, on_delete=models.PROTECT, null=True, blank=True)
+    category = models.ForeignKey(BlockCategory, on_delete=models.PROTECT, null=True, blank=True, verbose_name=_('Block category'))
     login_required = models.BooleanField(_("Login required"), help_text=_("If checked, only logged in users can view this page"), default=False)
     show_title = models.BooleanField(_("Show title"), help_text=_("If checked, show block title"), default=False)
 
